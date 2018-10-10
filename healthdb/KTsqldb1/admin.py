c3348714e9981f6d773f24036bec92a2c0837c9d
benkenobi007 <ktiwari998@gmail.com>
2018-10-11 02:37:04 +0530

    remove old db

--- healthdb/KTsqldb1/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Patient)
-admin.site.register(Doctor)
-admin.site.register(Appointment)
-admin.site.register(Diagnosis)
-admin.site.register(Treatment)
