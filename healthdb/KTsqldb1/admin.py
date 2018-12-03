18b474425bdbe0b91fddb1acfdbf04818bd3971a
benkenobi007 <ktiwari998@gmail.com>
2018-12-03 10:44:27 +0530

    Customized admin

--- healthdb/KTsqldb1/admin.py
+++ healthdb/KTsqldb1/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.admin import AdminSite
+from django.utils.translation import ugettext_lazy
+
+class MyAdminSite(AdminSite):
+    # Text to put at the end of each page's <title>.
+    site_title = ugettext_lazy('HealthDB admin')
+
+    # Text to put in each page's <h1> (and above login form).
+    site_header = ugettext_lazy('HealthDB administration')
+
+    # Text to put at the top of the admin index page.
+    index_title = ugettext_lazy('Site administration')
+
+admin_site = MyAdminSite()
 
 # Register your models here.
 admin.site.register(Patient)
@@ -9,3 +23,13 @@ admin.site.register(Diagnosis)
 admin.site.register(Treatment)
 admin.site.register(PatientCredentials)
 admin.site.register(DoctorCredentials)
+
+## Register models with custom admin website
+
+admin_site.register(Patient)
+admin_site.register(Doctor)
+admin_site.register(Appointment)
+admin_site.register(Diagnosis)
+admin_site.register(Treatment)
+admin_site.register(PatientCredentials)
+admin_site.register(DoctorCredentials)
--- healthdb/healthdb/urls.py
+++ healthdb/healthdb/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from KTsqldb1.admin import admin_site
 
 urlpatterns = [
     path('healthdb/',include('KTsqldb1.urls')),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),
 ]
